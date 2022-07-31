574a07c4a005d5a6b2f5400296bf7252240b42ee
felipe <felipeyukio182@gmail.com>
2022-07-31 15:19:23 -0300

    refactor: ajuste nos nomes das variaveis de ambiente

--- src/helpers/getDynamoClient.ts
+++ src/helpers/getDynamoClient.ts
@@ -4,8 +4,8 @@ function getDynamodbClient() {
     return new DynamoDBClient({
         region: "us-east-1",
         credentials: {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID_PROD,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_PROD
         }
     });
 }
